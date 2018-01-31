a60d470f45e3005565ea42c2e8b4dedeabc5315c
vileperson <25247569+vileperson@users.noreply.github.com>
2018-01-31 11:51:25 -0500

    tmux mods and updated unfinished script

--- linkFilesRev2.sh
+++ linkFilesRev2.sh
@@ -1,38 +1,59 @@
-#!/bin/bash
+#!/bin/zsh
 
 # variables
 
-gitdir=~/Git/dotfiles
-bakdir=~/Git/dotfilesbak
+gitdir=~/Git/dotfiles/
+bakdir=~/Git/dotfilesbak/
 linkdir=~/testdir
 homedir=~/
 curdir=~/Git/dotfiles
-curbakdir=~/test_old
+curbakdir=~/test_old/
 # functions
 
-fun1 () # handles files in current directory
+fun1 () # Setup directory for backing up files
 {
-	for f in .[^.]* ; do
-		if [ -f "$linkdir"/"$f" ]; then
-		mv "$linkdir"/"$f" "$curbakdir"/
+	if [ ! -d "$curbakdir" ]; then
+	mkdir "$curbakdir"
+	fi
+}
+
+fun2 () # handles files in current directory
+{
+	cd "$curdir"
+	for f in .[^.]*(^/) ; do
+		if [ -f "$linkdir""$f" ]; then
+#		echo "file already there"	
+		mv "$linkdir""$f"" ""$curbakdir"
 		fi
-		ln -s "$curdir"/"$f" "$linkdir"
+		ln -s "$curdir$f""$linkdir$f"
 	done
 }
 
-fun2 () #handles directories in current directory
+fun3 () #handles directories in current directory
 {
 	for d in  .*/ ; do
-		if [ ! -d "$linkdir"/"$d" ]; then
-		mkdir -p "$linkdir"/"$d"
+		if [ ! -d "$linkdir""$d" ]; then
+		mkdir -p "$linkdir""$d"
 		fi
-		if [ ! -d "$curbakdir"/"$d" ]; then
-		mkdir -p "$curbakdir"/"$d"
+		if [ ! -d "$curbakdir""$d" ]; then
+		mkdir -p "$curbakdir""$d"
 		fi
 	done
 }
 
-fun1
-#fun2
+fun4 () # script testing function
+{
+	cd "$curdir"
+	for f in .*(^/) ; do
+		echo "$f"
+		echo "$linkdir/$f"
+		echo "$curdir/$f"
+		ln -s "$curdir/$f $linkdir/$f"
+		done
+ 
+} 
 
+#fun1
+#fun2
+fun4
 
